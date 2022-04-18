a9f1fc19b1d03beb2efe8b601e5e6f1e2e68fa9a
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-04-18 16:14:13 -0400

    ex_manual note

--- src/ex_manual.py
+++ src/ex_manual.py
@@ -1,9 +1,9 @@
 """
-Example of how the CRC-based state hashing works for a simple class,
-implemented manually.
-
-NOTE: this file is just for reference. See ex_decorated.py for
+NOTE: this file is probably not what you want. See ex_decorated.py for
 the correct usage of the module.
+
+Example of how the CRC-based state hashing works for a simple class,
+*implemented manually*.
 
 The output of this file should be similar (but not exactly the same) as
 ex_decorated.py, since the objects are turned into bytes a little
